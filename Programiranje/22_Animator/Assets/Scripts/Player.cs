ca15921497d437cbc3c391c4d6f7d17a8498b7ac
Cwekii <35892622+Cwekii@users.noreply.github.com>
2023-10-05 13:52:41 +0200

    Cinemachine
    Nadograđen projekt od jučer (04. 10.) sa novom scenom, playerom, i assetima. Cinemachine player u sceni SampleScene i cutscena sa (broken) playerom u sceni Cutscene. !!!PROJEKT JE UPGRADEAN NA HDRP!!!

--- Programiranje/22_Animator/Assets/Scripts/Player.cs
+++ Programiranje/22_Animator/Assets/Scripts/Player.cs
@@ -1,67 +1,112 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //osnovne stvari za 3rd person controller
+    //essentials
+    public Transform cam;
     CharacterController controller;
-    public float MoveSpeed;
-    Vector2 movement;
-    bool running;
-
-    //stvari za skakanje, gravitacija itd
-    public float jumpHeight;
-    public float Gravity;
-    bool isGrounded;
-    Vector3 velocity;
-
-    //Animator
+    float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
     Animator anim;
+
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    //Movement
+    Vector2 movement;
+    public float walkSpeed;
+    public float sprintSpeed;
+    float trueSpeed;
+    bool Sprinting;
+
+    //Jumping
+    public float jumpHeight;
+    public float gravity;
+    bool isGrounded;
+    bool isJumping;
+    Vector3 velocity;
 
     private void Start()
     {
-        //ugasi cursor i samo assignaj CharacterController
+        trueSpeed = walkSpeed;
         controller = GetComponent<CharacterController>();
+
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
         anim = GetComponentInChildren<Animator>();
     }
 
     private void Update()
     {
-            //movement
+        isGrounded = Physics.CheckSphere(groundCheck.position, .1f, groundLayer);
+        anim.SetBool("IsGrounded", isGrounded);
 
-        //pretvori input u movement vektore
-        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-
-        //odredi smjer playera radi rotacije
-        Vector3 direction = new Vector3(movement.x, 0, movement.y).normalized;
-
-        if (direction.magnitude >= 0.1f)
-        {
-            controller.Move(direction * MoveSpeed * Time.deltaTime);
-            anim.SetFloat("Speed", 1); //kolko god sam da je vise od 0.1 da pièi
-        }
-        else
-        {
-            anim.SetFloat("Speed", 0); //ako player ne pièi, vrati animaciju na idle
-        }
-
-
-            //gravitacija
-
-        //generiramo sferu pod nogama da detektiramo jesmo na podu il ne
-        isGrounded = Physics.CheckSphere(transform.position, 0.1f, 1);
-
-        //drži velocity na odreðenoj vrijednosti
         if(isGrounded && velocity.y < 0)
         {
             velocity.y = -1;
         }
 
-        //vuci playera dole stalno
-        velocity.y += Gravity * Time.deltaTime;
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            trueSpeed = sprintSpeed;
+            Sprinting = true;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            trueSpeed = walkSpeed;
+            Sprinting = false;
+        }
+
+        anim.transform.localPosition = Vector3.zero;
+        anim.transform.localEulerAngles = Vector3.zero;
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector3 direction = new Vector3(movement.x, 0, movement.y).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * trueSpeed * Time.deltaTime);
+
+            if(Sprinting == true)
+            {
+                anim.SetFloat("Speed", 2);
+            }
+            else
+            {
+                anim.SetFloat("Speed", 1);
+            }
+        }
+        else
+        {
+            anim.SetFloat("Speed", 0);
+        }
+
+
+        //Jumping
+        //jebo pas mater neradi
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt((jumpHeight * 10) * -2f * gravity);
+            anim.SetBool("IsJumping", true);
+        }
+        else
+        {
+            anim.SetBool("IsJumping", false);
+        }
+
+        if (velocity.y > -20)
+        {
+            velocity.y += (gravity * 10) * Time.deltaTime;
+        }
+
         controller.Move(velocity * Time.deltaTime);
     }
 }
